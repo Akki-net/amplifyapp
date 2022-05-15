cffc308b91ac546af573c4186f470be0a8e5d901
unknown <akashsharma.zak@gmail.com>
2022-05-15 14:22:04 +0530

    changes in app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello From Akash
         </p>
         <a
           className="App-link"
